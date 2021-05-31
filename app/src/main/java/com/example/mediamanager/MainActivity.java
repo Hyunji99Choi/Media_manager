002d212a00efb1847478ae48cf81cb8a26ebbcee
Hyunji99Choi <tyg06229@gmail.com>
2021-06-01 00:53:24 +0900

    앱 꺼질때 스레드 관리

--- app/src/main/java/com/example/mediamanager/MainActivity.java
+++ app/src/main/java/com/example/mediamanager/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     Thread thread;
+    boolean running = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +28,35 @@ public class MainActivity extends AppCompatActivity {
                     Thread.sleep(5000); //5초 대기
                 }catch (InterruptedException e){ }
 
-                //메인화면인 gridview 액티비티 실행
-                startActivity(new Intent(MainActivity.this, MainGridView.class));
-                //표지 액티비티 종료
-                finish();
+                //running이 true 일때만 실행 -> false: 버튼클릭,창 닫기
+                if(running){
+                    //메인화면인 gridview 액티비티 실행
+                    startActivity(new Intent(MainActivity.this, MainGridView.class));
+                    //표지 액티비티 종료
+                    finish();
+                }
+
             }
         });
         thread.start();
     }
 
+    //표지화면에서 앱을 종료하면
+    //진행중이던 쓰레드에서 액티비티 호출 막기
+    //해주지 않으면 앱을 종료후에도
+    //5초 경과 후 메인화면이 켜짐.
+    @Override
+    protected void onStop() {
+        super.onStop();
+        running = false;
+    }
+
     // 닫기 버튼 클릭 콜백 함수
     public void close(View view){
         //버튼 클릭시 thread 취소(페이지가 두번 켜지는것을 방지)
-        thread.interrupt();
+        //후에 running 변수를 추가하면서 필요없어 졌다.
+        //thread.interrupt();
+        running = false;
 
         startActivity(new Intent(MainActivity.this, MainGridView.class));
         finish(); //표지 액티비티 종료
